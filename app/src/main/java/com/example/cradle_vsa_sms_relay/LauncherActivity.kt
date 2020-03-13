3e9b36b0e9949661bdace371ecf43279a2a14f5d
sachin <sraturi@sfu.ca>
2020-03-13 16:23:30 -0700

    added a login screen and basic UI

---
+++ app/src/main/java/com/example/cradle_vsa_sms_relay/LauncherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cradle_vsa_sms_relay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
--- app/src/main/java/com/example/cradle_vsa_sms_relay/MainActivity.kt
+++ app/src/main/java/com/example/cradle_vsa_sms_relay/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.cradle_vsa_sms_relay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupStartService()
+        setupStopService()
+    }
+
+    private fun setupStopService() {
+        findViewById<Button>(R.id.btnStartService).setOnClickListener{
+
+        }
+    }
+
+    private fun setupStartService() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
