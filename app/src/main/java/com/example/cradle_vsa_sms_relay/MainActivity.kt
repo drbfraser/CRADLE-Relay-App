d848c8a4d81431e5592c3fe84b78fc0690f9921d
sachin <sraturi@sfu.ca>
2020-03-14 13:17:55 -0700

    permissions setup done

--- app/src/main/java/com/example/cradle_vsa_sms_relay/MainActivity.kt
+++ app/src/main/java/com/example/cradle_vsa_sms_relay/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.cradle_vsa_sms_relay
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkpermissions()
         setupStartService()
         setupStopService()
     }
+
 
     private fun setupStopService() {
         findViewById<Button>(R.id.btnStopService).setOnClickListener{
@@ -20,5 +27,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupStartService() {
+    }
+
+
+    private fun checkpermissions() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ActivityCompat.requestPermissions(this, arrayOf( Manifest.permission.FOREGROUND_SERVICE,
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.RECEIVE_MMS),99)
+            } else{
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET,
+                    Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.RECEIVE_MMS),99)
+            }
+        } else{
+            //permission already granted
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==99){
+            //do whatever when permissions are granted
+        }
     }
 }
