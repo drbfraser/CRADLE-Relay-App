947400b218655759c574eb0e447f8558fd2f9f3a
sachin <sraturi@sfu.ca>
2020-04-11 00:29:05 -0700

    able to get sharedpref values

--- app/src/main/java/com/example/cradle_vsa_sms_relay/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/cradle_vsa_sms_relay/activities/SettingsActivity.kt
@@ -2,7 +2,7 @@ package com.example.cradle_vsa_sms_relay.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 import com.example.cradle_vsa_sms_relay.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -17,9 +17,27 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat(){
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val reuploadListKey = getString(R.string.reuploadListPrefKey)
+            val reuploadSwitchKey = getString(R.string.reuploadSwitchPrefKey)
+
+            val pref = findPreference<ListPreference>(reuploadListKey)
+            pref?.isEnabled =
+                PreferenceManager.getDefaultSharedPreferences(this.context).getBoolean(reuploadSwitchKey,false)
+            findPreference<SwitchPreferenceCompat>(reuploadSwitchKey)?.setOnPreferenceClickListener {preference ->
+                pref?.isEnabled =
+                    preference.sharedPreferences.getBoolean(reuploadSwitchKey,false)
+                true
+            }
+
         }
     }
 }
