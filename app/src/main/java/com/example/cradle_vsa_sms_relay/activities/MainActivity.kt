b3b8e3c02e0d9efb6d60d010dd1d9f79807a67fc
sachin <sraturi@sfu.ca>
2020-04-10 20:29:37 -0700

    added setting menu

--- app/src/main/java/com/example/cradle_vsa_sms_relay/activities/MainActivity.kt
+++ app/src/main/java/com/example/cradle_vsa_sms_relay/activities/MainActivity.kt
@@ -6,6 +6,9 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -47,6 +50,22 @@ class MainActivity : AppCompatActivity(),
         )
 
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settingMenu -> {
+                //newGame()
+                true
+            }else -> super.onOptionsItemSelected(item)
+
+        }
+
+        }
 
     private fun setuprecyclerview() {
 
