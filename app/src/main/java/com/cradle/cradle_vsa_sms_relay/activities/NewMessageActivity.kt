69a9ed1564c7966850b64feaa41240b07172187c
sraturi <sraturi@sfu.ca>
2020-05-31 00:57:52 -0700

    for #14 created a new message activity

---
+++ app/src/main/java/com/cradle/cradle_vsa_sms_relay/activities/NewMessageActivity.kt
@@ -0,0 +1,13 @@
+package com.cradle.cradle_vsa_sms_relay.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cradle.cradle_vsa_sms_relay.R
+
+class NewMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+    }
+}
