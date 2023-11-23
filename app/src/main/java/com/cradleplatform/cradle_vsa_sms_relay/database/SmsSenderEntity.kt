92d87f7534b60e92a357b4a289a087816cde91cb
Peter Fan <cfa60@sfu.ca>
2023-11-22 21:44:36 -0800

    Fixed detekt

--- app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/database/SmsSenderEntity.kt
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/database/SmsSenderEntity.kt
@@ -6,8 +6,10 @@ data class SmsSenderEntity(
     // phonenumber-requestcounter
     val id: String,
     val encryptedData: MutableList<String>?,
-    val responseCode: String?, // TODO: Consider making enum class for this
-    val phoneNumber: String?,  // TODO: Consider making a PhoneNumber class for unified format
+    // Consider making enum class for responseCode
+    val responseCode: String?,
+    // Consider making a PhoneNumber class for a unified format
+    val phoneNumber: String?,
     // unix stamp for when the callback was completed
     val timeReceived: Long,
     val totalMessages: Int,
