36a4c89584d42ba17eda428bfa12dde95a63849f
Sadaf Ahmadi <90568896+Saa142@users.noreply.github.com>
2024-02-25 19:23:05 -0800

    Completed checkmarks
    -added icons for 4 tasks
    -added check marks on top when each is completed
    -show completed message when all done

--- app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/adapters/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/adapters/MainRecyclerViewAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.cradleplatform.cradle_vsa_sms_relay.model.SmsRelayEntity
@@ -44,16 +45,28 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
         holder.reqConter.text = smsRelayEntity.getRequestIdentifier()
 
         if (smsRelayEntity.numFragmentsReceived == smsRelayEntity.totalFragmentsFromMobile) {
+            //to be deleted:Start
             holder.checkBox1.isChecked = true
             holder.checkBox2.isChecked = true
+            //end
+            holder.checkMark1.visibility = View.VISIBLE
+            holder.checkMark2.visibility = View.VISIBLE
         } else {
+            //to be deleted:Start
             holder.checkBox1.isChecked = false
             holder.checkBox2.isChecked = false
+            //end
         }
-        if (smsRelayEntity.isServerError == true || smsRelayEntity.isServerResponseReceived == true) {
+        if (smsRelayEntity.isServerError == true || smsRelayEntity.isServerResponseReceived) {
+            //to be deleted:Start
             holder.checkBox3.isChecked = true
             holder.checkBox1.isChecked = true
             holder.checkBox2.isChecked = true
+            //end
+
+            holder.checkMark3.visibility = View.VISIBLE
+            holder.checkMark1.visibility = View.VISIBLE
+            holder.checkMark2.visibility = View.VISIBLE
         } else {
             holder.checkBox3.isChecked = false
         }
@@ -63,10 +76,24 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
             holder.error.visibility = View.GONE
         }
         if (smsRelayEntity.smsPacketsToMobile.isEmpty() && smsRelayEntity.isServerResponseReceived) {
+            //to be deleted:Start
             holder.checkBox4.isChecked = true
             holder.checkBox1.isChecked = true
             holder.checkBox2.isChecked = true
             holder.checkBox3.isChecked = true
+            //end
+
+            holder.checkMark4.visibility = View.VISIBLE
+            holder.checkMark1.visibility = View.VISIBLE
+            holder.checkMark2.visibility = View.VISIBLE
+            holder.checkMark3.visibility = View.VISIBLE
+
+            holder.imageView1.alpha = 0.2F
+            holder.imageView2.alpha = 0.2F
+            holder.imageView3.alpha = 0.2F
+            holder.imageView4.alpha = 0.2F
+
+            holder.confirmationMessage.visibility = View.VISIBLE
         } else {
             holder.checkBox4.isChecked = false
         }
@@ -80,5 +107,14 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
         val phone: TextView = itemView.findViewById<TextView>(R.id.phone)
         val reqConter: TextView = itemView.findViewById<TextView>(R.id.requestCounter)
         val error: TextView = itemView.findViewById(R.id.serverErrorText)
+        val checkMark1: ImageView = itemView.findViewById(R.id.receivedMobileCheckMark)
+        val checkMark2: ImageView = itemView.findViewById(R.id.UploadServerCheckMark)
+        val checkMark3: ImageView = itemView.findViewById(R.id.receiveServerCheckMark)
+        val checkMark4: ImageView = itemView.findViewById(R.id.sentMobileCheckMark)
+        val imageView1: ImageView = itemView.findViewById(R.id.receivedMobile)
+        val imageView2: ImageView = itemView.findViewById(R.id.UploadServer)
+        val imageView3: ImageView = itemView.findViewById(R.id.receiveServer)
+        val imageView4: ImageView = itemView.findViewById(R.id.sentMobile)
+        val confirmationMessage: TextView = itemView.findViewById(R.id.completed)
     }
 }
