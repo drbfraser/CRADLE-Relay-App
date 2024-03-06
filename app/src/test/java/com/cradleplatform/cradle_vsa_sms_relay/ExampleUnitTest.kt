6e5c8b633922b6d3ff4159e9c0c714161e3c5c1a
Ali Nanji <nanjialimudabir@gmail.com>
2024-03-06 15:41:30 -0800

    Moving test files to correct folders

--- app/src/test/java/com/cradleplatform/cradle_vsa_sms_relay/ExampleUnitTest.kt
+++ app/src/test/java/com/cradleplatform/cradle_vsa_sms_relay/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
 package com.cradleplatform.cradle_vsa_sms_relay
 
-import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Test
+import com.cradleplatform.cradle_vsa_sms_relay.
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,6 +10,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private val messageReceiver: MessageReceiver? = null
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
