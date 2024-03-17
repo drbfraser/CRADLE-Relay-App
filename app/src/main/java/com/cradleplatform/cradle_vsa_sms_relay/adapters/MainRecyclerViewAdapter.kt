38cafbd7eef5d275c2ce60032986301c16798228
Sadaf Ahmadi <90568896+Saa142@users.noreply.github.com>
2024-03-17 12:34:46 -0700

    removed previous design of sms relay
    -removed the already existing design of the app

--- app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/adapters/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/adapters/MainRecyclerViewAdapter.kt
@@ -22,25 +22,22 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
 
      private var sms: List<SmsRelayEntity> = ArrayList()
      private var phoneList: MutableList<String> = ArrayList()
+    // Constants for alpha values
+    private val ALPHA_FULL = 1F
+    private val ALPHA_DIM = 0.2F
     init {
         // Add "ALL" to the initial phoneList
         phoneList.add("All")
     }
-    // Constants for alpha values
-    private val ALPHA_FULL = 1F
-    private val ALPHA_DIM = 0.2F
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SMSViewHolder {
         val v: View =
             LayoutInflater.from(parent.context)
                 .inflate(R.layout.sms_recycler_item_layout, parent, false)
         return SMSViewHolder(v)
     }
-
     override fun getItemCount(): Int {
         return sms.size
     }
-
     fun setRelayList(smsRelayEntities: List<SmsRelayEntity>) {
         this.sms = smsRelayEntities
         phoneList.clear()
@@ -48,7 +45,6 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
         phoneList.addAll(smsRelayEntities.map { it.getPhoneNumber() }.distinct())
         notifyDataSetChanged()
     }
-
     fun getPhoneNumbers(): List<String> {
         return phoneList
     }
@@ -59,75 +55,37 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
         val smsRelayEntity: SmsRelayEntity = sms[position]
         val numFragmentsReceived = smsRelayEntity.numFragmentsReceived
         val totalFragmentsFromMobile = smsRelayEntity.totalFragmentsFromMobile
+        holder.receivedDateTime.text = smsRelayEntity.getDateAndTime()
+        holder.duration.text = ""
+        holder.phone.text = smsRelayEntity.getPhoneNumber()
 
         if (numFragmentsReceived <= totalFragmentsFromMobile) {
-            holder.receivingMobile.text = "Receiving $numFragmentsReceived out of $totalFragmentsFromMobile"
-            if (numFragmentsReceived == totalFragmentsFromMobile) {
-                holder.receivingMobile.text = "Received all messages"
-                holder.checkMark1.visibility = View.VISIBLE
-                holder.imageView1.alpha = ALPHA_DIM
-            }
+            holder.receivingMobile.text = "Receiving $numFragmentsReceived out of $totalFragmentsFromMobile messages"
         }
-        holder.receivedDateTime.text = smsRelayEntity.getDateAndTime()
-        holder.duration.text = smsRelayEntity.getDuration()
-        holder.phone.text = smsRelayEntity.getPhoneNumber()
-//        holder.receiveMobile.visibility = View.VISIBLE
-
-
-        if (smsRelayEntity.numFragmentsReceived == smsRelayEntity.totalFragmentsFromMobile) {
-            //to be deleted:Start
-            holder.checkBox1.isChecked = true
-            holder.checkBox2.isChecked = true
-            //end
+        if (numFragmentsReceived == totalFragmentsFromMobile) {
+            holder.receivingMobile.text = "Received all messages"
             holder.checkMark1.visibility = View.VISIBLE
             holder.checkMark2.visibility = View.VISIBLE
             holder.imageView1.alpha = ALPHA_DIM
             holder.imageView2.alpha = ALPHA_DIM
-//            holder.uploadServer.visibility = View.VISIBLE
-//            holder.receiveMobile.visibility = View.INVISIBLE
-
-        } else {
-            //to be deleted:Start
-            holder.checkBox1.isChecked = false
-            holder.checkBox2.isChecked = false
-            //end
-            holder.checkMark1.visibility = View.INVISIBLE
-            holder.checkMark2.visibility = View.INVISIBLE
-            holder.imageView1.alpha = ALPHA_FULL
-            holder.imageView2.alpha = ALPHA_FULL
+            holder.duration.text = smsRelayEntity.getDuration()
         }
         if (smsRelayEntity.isServerError == true || smsRelayEntity.isServerResponseReceived == true) {
-            //to be deleted:Start
-            holder.checkBox3.isChecked = true
-            holder.checkBox1.isChecked = true
-            holder.checkBox2.isChecked = true
-            //end
             holder.imageView1.alpha = ALPHA_DIM
             holder.imageView2.alpha = ALPHA_DIM
             holder.imageView3.alpha = ALPHA_DIM
-
-//            holder.uploadServer.visibility = View.INVISIBLE
-//            holder.receiveServer.visibility = View.VISIBLE
-
             holder.checkMark3.visibility = View.VISIBLE
             holder.checkMark1.visibility = View.VISIBLE
             holder.checkMark2.visibility = View.VISIBLE
         } else {
-            holder.checkBox3.isChecked = false
-            holder.checkMark3.visibility = View.INVISIBLE
-            holder.imageView3.alpha = ALPHA_FULL
+           // holder.imageView3.alpha = ALPHA_FULL
         }
-        if (smsRelayEntity.isServerError == true) {
-            holder.error.visibility = View.VISIBLE
-        } else {
-            holder.error.visibility = View.GONE
-        }
+//        if (smsRelayEntity.isServerError == true) {
+//            //holder.error.visibility = View.VISIBLE
+//        } else {
+//            //holder.error.visibility = View.GONE
+//        }
         if (smsRelayEntity.smsPacketsToMobile.isEmpty() && smsRelayEntity.isServerResponseReceived) {
-            holder.checkBox4.isChecked = true
-            holder.checkBox1.isChecked = true
-            holder.checkBox2.isChecked = true
-            holder.checkBox3.isChecked = true
-            //end
             holder.checkMark4.visibility = View.VISIBLE
             holder.checkMark1.visibility = View.VISIBLE
             holder.checkMark2.visibility = View.VISIBLE
@@ -136,46 +94,29 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
             holder.imageView2.alpha = ALPHA_DIM
             holder.imageView3.alpha = ALPHA_DIM
             holder.imageView4.alpha = ALPHA_DIM
-//            holder.receiveServer.visibility = View.INVISIBLE
-//            holder.receiveMobile.visibility = View.INVISIBLE
-//            holder.uploadServer.visibility = View.INVISIBLE
-//            holder.completedMessage.visibility = View.VISIBLE
         } else {
-            holder.checkBox4.isChecked = false
-            holder.checkMark4.visibility = View.INVISIBLE
-//            holder.completedMessage.visibility = View.INVISIBLE
-            holder.imageView4.alpha = ALPHA_FULL
+            //holder.checkMark4.visibility = View.INVISIBLE
+            //holder.imageView4.alpha = ALPHA_FULL
+        }
+
+        if (smsRelayEntity.isCompleted){
+            holder.duration.text = smsRelayEntity.getDuration()
+            holder.receivingMobile.text = "Completed"
+            holder.checkMark4.visibility = View.VISIBLE
+            holder.checkMark1.visibility = View.VISIBLE
+            holder.checkMark2.visibility = View.VISIBLE
+            holder.checkMark3.visibility = View.VISIBLE
+            holder.imageView1.alpha = ALPHA_DIM
+            holder.imageView2.alpha = ALPHA_DIM
+            holder.imageView3.alpha = ALPHA_DIM
+            holder.imageView4.alpha = ALPHA_DIM
         }
     }
 
-    private fun serverResponseReceived(holder: SMSViewHolder) {
-        //to be deleted:Start
-        holder.checkBox4.isChecked = true
-        holder.checkBox1.isChecked = true
-        holder.checkBox2.isChecked = true
-        holder.checkBox3.isChecked = true
-        //end
-        holder.checkMark4.visibility = View.VISIBLE
-        holder.checkMark1.visibility = View.VISIBLE
-        holder.checkMark2.visibility = View.VISIBLE
-        holder.checkMark3.visibility = View.VISIBLE
-        holder.imageView1.alpha = ALPHA_DIM
-        holder.imageView2.alpha = ALPHA_DIM
-        holder.imageView3.alpha = ALPHA_DIM
-        holder.imageView4.alpha = ALPHA_DIM
-//        holder.receiveServer.visibility = View.INVISIBLE
-//        holder.receiveMobile.visibility = View.INVISIBLE
-//        holder.uploadServer.visibility = View.INVISIBLE
-//        holder.completedMessage.visibility = View.VISIBLE
 
-    }
     class SMSViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val checkBox1: CheckBox = itemView.findViewById<CheckBox>(R.id.checkBox1)
-        val checkBox2: CheckBox = itemView.findViewById<CheckBox>(R.id.checkBox2)
-        val checkBox3: CheckBox = itemView.findViewById<CheckBox>(R.id.checkBox3)
-        val checkBox4: CheckBox = itemView.findViewById<CheckBox>(R.id.checkBox4)
         val phone: TextView = itemView.findViewById<TextView>(R.id.phone)
-        val error: TextView = itemView.findViewById(R.id.serverErrorText)
+        //val error: TextView = itemView.findViewById(R.id.serverErrorText)
         val checkMark1: ImageView = itemView.findViewById(R.id.receivedMobileCheckMark)
         val checkMark2: ImageView = itemView.findViewById(R.id.UploadServerCheckMark)
         val checkMark3: ImageView = itemView.findViewById(R.id.receiveServerCheckMark)
@@ -188,10 +129,6 @@ RecyclerView.Adapter<MainRecyclerViewAdapter.SMSViewHolder>() {
         val imageView2: ImageView = itemView.findViewById(R.id.UploadServer)
         val imageView3: ImageView = itemView.findViewById(R.id.receiveServer)
         val imageView4: ImageView = itemView.findViewById(R.id.sentMobile)
-//        val completedMessage: TextView = itemView.findViewById(R.id.completed)
-//        val receiveMobile: TextView = itemView.findViewById(R.id.receivingMobile)
-//        val uploadServer: TextView = itemView.findViewById(R.id.uploadingServer)
-//        val receiveServer: TextView = itemView.findViewById(R.id.receivingServer)
         val receivingMobile: TextView = itemView.findViewById(R.id.sendingMobile)
         val receivedDateTime: TextView = itemView.findViewById<TextView>(R.id.receivedDateTime)
         val duration: TextView = itemView.findViewById<TextView>(R.id.duration)
