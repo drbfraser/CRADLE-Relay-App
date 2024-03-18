d4651fc0b0c646ded2c3f1b716bf0d992a24156d
js <50718474+jarell-santella@users.noreply.github.com>
2024-03-18 12:43:46 -0400

    fix(lint): suppress lint

--- app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/repository/HttpsRequestRepository.kt
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/repository/HttpsRequestRepository.kt
@@ -104,6 +104,7 @@ class HttpsRequestRepository(
                     else {
                         JSONObject(errorBody.string()).getString("message")
                     }
+                    @Suppress("ForbiddenComment")
                     // TODO: Add retry functionality here as well and look at why we are doing the
                     //  below on failure
                     synchronized(this@HttpsRequestRepository) {
