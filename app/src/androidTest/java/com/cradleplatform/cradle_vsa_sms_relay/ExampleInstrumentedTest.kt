08dbb60dad971f18bbcd8cab866a7fd40a2f2a16
Ali Nanji <nanjialimudabir@gmail.com>
2024-04-01 17:14:55 -0700

    Basic example UI test set up

--- app/src/androidTest/java/com/cradleplatform/cradle_vsa_sms_relay/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cradleplatform/cradle_vsa_sms_relay/ExampleInstrumentedTest.kt
@@ -1,8 +1,17 @@
 package com.cradleplatform.cradle_vsa_sms_relay
 
+import android.os.Build
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import com.cradleplatform.cradle_vsa_sms_relay.activities.MainActivity
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,10 +22,34 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    private val permissions = arrayOf(
+        android.Manifest.permission.RECEIVE_SMS,
+        android.Manifest.permission.READ_SMS,
+        android.Manifest.permission.SEND_SMS,
+        android.Manifest.permission.INTERNET
+    )
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Rule @JvmField
+    val grantPermissionRule: GrantPermissionRule =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            GrantPermissionRule.grant(*permissions, android.Manifest.permission.FOREGROUND_SERVICE)
+        } else {
+            GrantPermissionRule.grant(*permissions, android.Manifest.permission.READ_PHONE_STATE)
+        }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.cradle_vsa_sms_relay", appContext.packageName)
+        assertEquals("com.cradleplatform.cradle_vsa_sms_relay", appContext.packageName)
+    }
+
+    @Test
+    fun testingNewMessage() {
+        onView(withText("START SERVICE")).check(matches(isDisplayed()))
     }
 }
