6b2e8e8ed40e43173ae8d420075141b4da771390
Ali Nanji <nanjialimudabir@gmail.com>
2024-04-05 12:42:14 -0700

    Basic test structure working. Can see new item in notifications list when an SMS message broadcast is sent to device

--- app/src/androidTest/java/com/cradleplatform/cradle_vsa_sms_relay/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cradleplatform/cradle_vsa_sms_relay/ExampleInstrumentedTest.kt
@@ -1,6 +1,10 @@
 package com.cradleplatform.cradle_vsa_sms_relay
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.util.Log
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -10,6 +14,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import com.cradleplatform.cradle_vsa_sms_relay.activities.MainActivity
+import com.cradleplatform.cradle_vsa_sms_relay.broadcast_receiver.MessageReceiver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -48,8 +55,20 @@ class ExampleInstrumentedTest {
         assertEquals("com.cradleplatform.cradle_vsa_sms_relay", appContext.packageName)
     }
 
+    @OptIn(ExperimentalStdlibApi::class)
     @Test
     fun testingNewMessage() {
-        onView(withText("START SERVICE")).check(matches(isDisplayed()))
+        val pduHex = "00000b915155255155f400004240400064942b23b0586b280d1299c5160c0683c1602d182cd69abedb6569d84d7eb7a9653c1d"
+        val pduByteArray = pduHex.hexToByteArray()
+        val pduByteArray2D = arrayOf<Any>(pduByteArray)
+
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("smsto:+15555215556")
+            putExtra("pdus", pduByteArray2D)
+        }
+
+        val smsMessage = MessageReceiver(getApplicationContext(), CoroutineScope(Dispatchers.Default))
+        smsMessage.onReceive(getApplicationContext(), intent)
+        onView(withText("+15555215554")).check(matches(isDisplayed()))
     }
 }
