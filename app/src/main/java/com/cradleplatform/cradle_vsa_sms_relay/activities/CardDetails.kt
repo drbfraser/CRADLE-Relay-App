f43bdfc21d0b7e7825eebe805c2c5878a54c9ca1
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-06-03 19:12:22 -0700

    Added expandable view with dummy data

--- app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/activities/CardDetails.kt
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/activities/CardDetails.kt
@@ -3,10 +3,17 @@ package com.cradleplatform.cradle_vsa_sms_relay.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
+import com.cradleplatform.cradle_vsa_sms_relay.adapters.ExpandableListData.data
 import android.widget.TextView
 import com.cradleplatform.cradle_vsa_sms_relay.R
+import com.cradleplatform.cradle_vsa_sms_relay.adapters.DetailsExpandableListAdapter
 
 class CardDetails : AppCompatActivity() {
+    private var expandableListView: ExpandableListView? = null
+    private var adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
@@ -20,18 +27,40 @@ class CardDetails : AppCompatActivity() {
             finish()
         }
 
-        val textDate: TextView = findViewById(R.id.textDate)
-        val textPhoneNumber: TextView = findViewById(R.id.textPhoneNumber)
-        val durationText: TextView = findViewById(R.id.durationtext)
-
-
         val date: String? = intent.getStringExtra("date")
         val phoneNumber: String? = intent.getStringExtra("phoneNumber")
         val duration: String? = intent.getStringExtra("duration")
+
+//        val textDate: TextView = findViewById(R.id.textDate)
+//        val textPhoneNumber: TextView = findViewById(R.id.textPhoneNumber)
+//        val durationText: TextView = findViewById(R.id.durationtext)
+        expandableListView = findViewById(R.id.detailsList)
+        if (expandableListView != null) {
+            val listData = data
+            titleList = ArrayList(listData.keys)
+            adapter = DetailsExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(adapter)
+//            expandableListView!!.setOnGroupExpandListener { groupPosition ->
+//                Toast.makeText(
+//                    applicationContext,
+//                    (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+//                Toast.makeText(
+//                    applicationContext,
+//                    (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+
+            }
+
+
         // Retrieve other data if passed
 
-        textDate.text = "Date: $date"
-        textPhoneNumber.text = "Phone Number: $phoneNumber"
-        durationText.text = "Duration: $duration"
+//        textDate.text = "Date: $date"
+//        textPhoneNumber.text = "Phone Number: $phoneNumber"
+//        durationText.text = "Duration: $duration"
     }
 }
---
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/adapters/DetailsExpandableListAdapter.kt
@@ -0,0 +1,75 @@
+package com.cradleplatform.cradle_vsa_sms_relay.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import com.cradleplatform.cradle_vsa_sms_relay.R
+
+class DetailsExpandableListAdapter(private val context: Context,
+                                   private val groupList: List<String>,
+                                   private val childList: HashMap<String, List<String>>)
+    : BaseExpandableListAdapter() {
+    override fun getGroupCount(): Int {
+        return this.groupList.size
+    }
+
+    override fun getChildrenCount(listPosition: Int): Int {
+        return this.childList[groupList[listPosition]]?.size ?: 0
+    }
+
+    override fun getGroup(listPosition: Int): Any {
+        return this.groupList[listPosition]
+    }
+
+    override fun getChild(listPosition: Int, expandableListPosition: Int): Any {
+        return this.childList[this.groupList[listPosition]]?.get(expandableListPosition) ?:
+        "Group does not exist"
+    }
+
+    override fun getGroupId(listPosition: Int): Long {
+        return listPosition.toLong()
+    }
+
+    override fun getChildId(listPosition: Int, expandableListPosition: Int): Long {
+        return expandableListPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+       return false
+    }
+
+    override fun getGroupView(listPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        if(convertView == null){
+            val layoutInflater =
+                this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.details_list_group, null)
+        }
+        val listTitle = getGroup(listPosition) as String
+        val listTitleTextView = convertView!!.findViewById<TextView>(R.id.listTitle)
+        listTitleTextView.text = listTitle
+        return convertView
+    }
+
+    override fun getChildView(listPosition: Int, expandableListPosition: Int, isLastChild: Boolean,
+                              convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        if(convertView == null){
+            val layoutInflater =
+                this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.details_list_item, null)
+        }
+        val expandedListText = getChild(listPosition, expandableListPosition) as String
+        val expandedListTextView = convertView!!.findViewById<TextView>(R.id.expandedListItemText)
+        expandedListTextView.text = expandedListText
+        return convertView
+
+    }
+
+    override fun isChildSelectable(listPosition: Int, p1: Int): Boolean {
+        return true
+    }
+}
