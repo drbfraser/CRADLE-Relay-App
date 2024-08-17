538750e9ba027d78f793b480bc6949b2ca207b62
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-08-17 13:32:47 -0700

    Removed unused imports

--- app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/view_model/DetailsViewModel.kt
+++ app/src/main/java/com/cradleplatform/cradle_vsa_sms_relay/view_model/DetailsViewModel.kt
@@ -1,21 +1,11 @@
 package com.cradleplatform.cradle_vsa_sms_relay.view_model
 
 import android.app.Application
-import android.provider.Telephony.Sms
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
 import com.cradleplatform.cradle_vsa_sms_relay.dagger.MyApp
 import com.cradleplatform.cradle_vsa_sms_relay.model.SmsRelayEntity
 import com.cradleplatform.cradle_vsa_sms_relay.repository.SmsRelayRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DetailsViewModel(application: Application): AndroidViewModel(application) {
